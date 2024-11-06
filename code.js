bae1ffbb1fe21b838737084b4c1de201957036ce
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-06 13:59:03 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,33 @@
 function permutationSort(a) {
-    for(let i = 0; i < a.length; i++) {
-        for(let j = i + 1; j < a.length; j++) {
-          //console.log("i = " + a[i]);
-          //console.log("j = " + a[j]);
-          //console.log(a);
-          if(a[i] > a[j]) {
-            let tmp = a[j];
-            a[j] = a[i];
-            a[i] = tmp;
-          }
-          
-        }
+    let counter = 0;
+    let zeroCounter = 0;
+    let alen = a.length;
+    while(counter < alen) {
+      if(a[counter] == 1) {
+        zeroCounter++;
+      }
+      counter++;
     }
+    if(zeroCounter == alen) {
+      return a;
+    }
+    else {
+      for(let i = 0; i < a.length; i++) {
+          for(let j = i + 1; j < a.length; j++) {
+            //console.log("i = " + a[i]);
+            //console.log("j = " + a[j]);
+            //console.log(a);
+            if(a[i] > a[j]) {
+              let tmp = a[j];
+              a[j] = a[i];
+              a[i] = tmp;
+            }
             
-    return a;
+          }
+      }
+              
+      return a;
+    }
 }
 
 //let array = [2, 4, 3, 1, 5];
