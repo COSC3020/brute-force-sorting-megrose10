d7baef57842c224978e80e353984a97575d6174b
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-12-04 18:40:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,38 +1,86 @@
 function permutationSort(a) {
-    let counter = 0;
-    let zeroCounter = 0;
-    let alen = a.length;
-    if(alen==0){
-      return a;
+    let isSorted = false;
+    let permNum = 1;
+    //let numToMove = 0;
+    if(checkSort(a) == true) {
+        console.log("already sorted");
     }
-    while(counter < alen) {
-      if(a[counter] == 1) {
-        zeroCounter++;
-      }
-      counter++;
-    }
-    if(zeroCounter == alen) {
-      return a;
-    }
-    else {
-      for(let i = 0; i < a.length; i++) {
-          for(let j = i + 1; j < a.length; j++) {
-            //console.log("i = " + a[i]);
-            //console.log("j = " + a[j]);
-            //console.log(a);
-            if(a[i] > a[j]) {
-              let tmp = a[j];
-              a[j] = a[i];
-              a[i] = tmp;
+    else{
+        while(isSorted == false) {
+            let numToMove = 0;
+            //let num = 0;
+            for(let i = 1; i < a.length; i++) {
+                let tmp = a[numToMove];
+                a[numToMove] = a[i];
+                a[i] = tmp;
+                permNum++;
+                numToMove++;
+                if(checkSort(a) == true) {
+                    isSorted = true;
+                    break;
+                    //i = array.length;
+                }
+                //num++;
+                //console.log(num + ". "+ array);
+                //permNum++;
+                //numToMove++;
+                // for(let j = i + 1; j < array.length; j++){
+                //     let tmp = array[i];
+                //     array[i] = array[j];
+                //     array[j] = tmp; 
+                //     if(checkSort(array) == true) {
+                //         isSorted = true;
+                //     }
+                //     console.log(array);
+                //     permNum += 1;
+                // }
             }
-            
-          }
-      }
-              
-      return a;
+        }
     }
+    //console.log("Number of permutations: " + permNum);
+    //console.log("Sorted: " + a);
+    return permNum;
 }
 
-//let array = [2, 4, 3, 1, 5];
-//array = permutationSort(array);
-//console.log(array);
+function checkSort(array) {
+
+    let isSorted = false;
+
+    for(let i = 0; i < array.length; i++) {
+        for(let j = i + 1; j < array.length; j++){
+            if(array[i] > array[j]) {
+                isSorted = false;
+                //console.log("this is false");
+                return false;
+            }
+            else {
+                //console.log("this is true");
+                isSorted = true;
+            }
+        }
+    }
+    //console.log(array);
+    //console.log(isSorted);
+    return true;
+}
+
+//test for isSorted
+// let array = [1, 2, 3];
+// if(checkSort(array) == true) {
+//     console.log("works! for [1, 2, 3]")
+// }
+// let array1 = [1, 3, 2, 4];
+// if(checkSort(array1) == false) {
+//     console.log("works! for [1, 3, 2, 4]");
+// }
+// let array2 = [1, 3, 2, 4, 5, 2, 4];
+// if(checkSort(array1) == false) {
+//     console.log("works! for [1, 3, 2, 4, 5, 2, 4]");
+// }
+
+//test for generating permutations
+//let array = [1, 2, 3];
+//permutationSort(array);
+//let array1 = [3, 2, 1];
+//let array1 = [3, 2, 5, 1, 4];
+//permutationSort(array1);
